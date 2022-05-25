58a80ff3269a711ce0b5351424de50ef85360976
isyanavalda <100085573+isyanavalda@users.noreply.github.com>
2022-05-25 09:15:33 +0700

    Create kontaktambah.php

---
+++ kontaktambah.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> Form Input </title>
+</head>
+<body>
+<form method="post" action="kontaksimpan.php">
+<table>
+<tr><td>NAMA</td><td><input type="text" name="nama"></td></tr>
+<tr><td>JENIS KELAMIN</td><td><input
+<input type="radio" name="jenis_kelamin" value="L"> Laki laki
+<input type="radio" name="jenis_kelamin" value="L"> Perempuan
+</td></tr>
+<tr><td>EMAIL</td><td><input type="text" name="email"></td></tr>
+<tr><td>ALAMAT</td><td><input type="text" name="alamat"></td></tr>
+<tr><td>KOTA</td><td><input type="text" name="kota"></td></tr>
+<tr><td>PESAN</td><td><input type="text" name="pesan"></td></tr>
+<tr><td colspan="2"><button type="submit" value="simpan">SIMPAN</button></td></tr>
+</table>
+</form>
+</body>
+</html>
